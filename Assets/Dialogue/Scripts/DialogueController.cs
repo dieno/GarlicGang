ad0efed468a887b98d5bbd40b272f1bf4218411c
XCVG <xcvgvcx@gmail.com>
2018-03-07 17:06:59 -0800

    json.net and stuff

---
+++ Assets/Dialogue/Scripts/DialogueController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public class DialogueController : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        LoadDialogue("coldopen");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void LoadDialogue(string dialogueName)
+    {
+        TextAsset ta = Resources.Load<TextAsset>("dData/" + dialogueName);
+        JObject jo = JObject.Parse(ta.text);
+        Debug.Log(jo);
+    }
+}
