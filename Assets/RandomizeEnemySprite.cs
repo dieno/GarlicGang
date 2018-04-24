cb73e227eb25a8490ee3ef456eadaa4e9fdc52df
Ryan Dieno <ryan.dieno@gmail.com>
2018-04-23 22:42:42 -0700

    Changed player sprite and created enemy prefab with randomized sprite

---
+++ Assets/RandomizeEnemySprite.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeEnemySprite : MonoBehaviour {
+
+    public Sprite[] enemySprites;
+
+	// Use this for initialization
+	void Start () {
+        if(enemySprites.Length > 0)
+        {
+            int index = Random.Range(0, enemySprites.Length - 1);
+
+            GetComponent<SpriteRenderer>().sprite = enemySprites[index];
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
